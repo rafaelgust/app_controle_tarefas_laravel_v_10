db5e6a622f84230d112d26b2fb1171e5f1b08c7f
Rafael Augusto <rafael.appdevs@gmail.com>
2025-08-08 11:24:19 -0300

    adiciona notificação de redefinição de senha e método correspondente no modelo User

---
+++ app/Notifications/RedefinirSenhaNotification.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Lang;
+
+class RedefinirSenhaNotification extends Notification
+{
+    use Queueable;
+
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail($notifiable)
+    {
+        $url = url(config('app.url') . route('password.reset', [
+            'token' => $this->token,
+            'name' => $notifiable->name,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        $saudacao = sprintf('Olá, %s!', $notifiable->name);
+
+        return (new MailMessage)
+            ->subject(Lang::get('Notificação de Redefinição de Senha'))
+            ->greeting($saudacao)
+            ->line(Lang::get('Você está recebendo este e-mail porque recebemos uma solicitação de redefinição de senha para sua conta.'))
+            ->action(Lang::get('Redefinir Senha'), $url)
+            ->line(Lang::get('Este link para redefinição de senha irá expirar em :count minutos.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+            ->line(Lang::get('Se você não solicitou uma redefinição de senha, nenhuma ação adicional é necessária.'))
+            ->salutation(Lang::get('Atenciosamente,') . PHP_EOL . config('app.name'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
